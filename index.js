1f9a95d39ab8b7abb03ea0d0c9497380083e467b
Leah Hill <leah@verysandwich.com>
2018-08-07 00:45:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function theBeatlesPlay(musicians, insturments){
   for (let i = 0; i < 25; i++) {
     if (i === 1) {
-      array.push("John Lennon plays guitar")}
+     return array
+    }
   }
-}
+  }
+  
