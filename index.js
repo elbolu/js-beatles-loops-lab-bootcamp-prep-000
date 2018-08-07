f99681f2d578e428253bf3d18320a27b4c8b3372
Leah Hill <leah@verysandwich.com>
2018-08-07 00:41:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, insturments){
   for (let i = 0; i < 25; i++) {
     if (i === 1) {
-      array.push("I am 1 strange loop.")
+      array.push("John Lennon plays guitar")
     } else {
       array.push(`I am ${i} strange loops.`)
     }
